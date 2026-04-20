7f17b226db354b4f68f408d24357716e578d980f
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-04-21 06:51:07 +0800

    Update Threading.pyi

--- src/System/Windows/Threading.pyi
+++ src/System/Windows/Threading.pyi
@@ -2,13 +2,13 @@
 https://learn.microsoft.com/en-us/dotnet/api/system.windows.threading
 """
 
-from typing import Final, Tuple, Unpack
+from typing import Final, Optional, Tuple, Unpack
 
 from System import Delegate, Object
 
 class Dispatcher(Object):
 	# incomplete
-	def Invoke[RT, *AT](self, method: Delegate[RT, Unpack[AT]], args: Tuple[Unpack[AT]]) -> RT: ...
+	def Invoke[RT, *AT](self, method: Delegate[RT, Unpack[AT]], args: Tuple[Unpack[AT]]) -> Optional[RT]: ...
 
 class DispatcherObject(Object):
 	def __init__(self): # protected
